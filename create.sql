b298b31645c10210133e18bd7a7d0a25ab5fbb08
Lukom <lukom_wro@o2.pl>
2011-11-27 19:57:43 +0100

    Poprawka sql

--- create.sql
+++ create.sql
@@ -3,15 +3,17 @@ CREATE TABLE category (
 	id INT UNSIGNED NOT NULL PRIMARY KEY,
 	name VARCHAR(255)
 );
-
 DROP TABLE IF EXISTS article;
 CREATE TABLE article (
 	id INT UNSIGNED NOT NULL PRIMARY KEY,
 	name VARCHAR(255)
 );
-
 DROP TABLE IF EXISTS articlecategory;
-CREATE TABLE articlecategory (
-	article INT UNSIGNED NOT NULL,
-	category INT UNSIGNED NOT NULL
+CREATE TABLE article_category (
+	id_article INT UNSIGNED NOT NULL,
+	id_category INT UNSIGNED NOT NULL,
+    INDEX (id_article),
+    FOREIGN KEY (id_article) REFERENCES article(id),
+    INDEX (id_category),
+    FOREIGN KEY (id_category) REFERENCES category(id),
 );

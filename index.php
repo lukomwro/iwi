eff2278f9d63d3075eaae63e5a3479212664e46d
Lukom <lukom_wro@o2.pl>
2011-11-28 19:59:56 +0100

    pobranie danych pdo

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 try {
     $db = new PDO('mysql:db=wiki;host=localhost', 'iwi', 'iwi');
+    foreach ($db->query('SELECT * FROM articles') as $row) {
+        var_dump($row);
+    }
 } catch (PDOException $ex) {
     echo 'Connection failed: ' . $e->getMessage();
 }

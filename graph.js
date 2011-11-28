3d6df25deae528a441a0329251d08933fd98bddf
Lukom <lukom_wro@o2.pl>
2011-11-28 21:52:43 +0100

    Dodanie kilka nodow

--- graph.js
+++ graph.js
@@ -5,46 +5,56 @@ var w = 960,
 var vis = d3.select("#chart").append("svg:svg")
     .attr("width", w)
     .attr("height", h);
+(function(d){
+    var nodes = d.getElementsByClassName('nodes'),
+        nodesLn = nodes.length, i;
+    for (i=0; i<nodes; i++) {
+        nodes[i].onclick = function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log(event);
+            d3.json(event.target.getAttribute('src'), function(json) {
+                var force = d3.layout.force()
+                    .charge(-120)
+                    .linkDistance(30)
+                    .nodes(json.nodes)
+                    .links(json.links)
+                    .size([w, h])
+                    .start();
 
-d3.json("ajax.php?limit=100", function(json) {
-  var force = d3.layout.force()
-      .charge(-120)
-      .linkDistance(30)
-      .nodes(json.nodes)
-      .links(json.links)
-      .size([w, h])
-      .start();
+                var link = vis.selectAll("line.link")
+                    .data(json.links)
+                    .enter().append("svg:line")
+                    .attr("class", "link")
+                    .style("stroke-width", function(d) { return Math.sqrt(d.value); })
+                    .attr("x1", function(d) { return d.source.x; })
+                    .attr("y1", function(d) { return d.source.y; })
+                    .attr("x2", function(d) { return d.target.x; })
+                    .attr("y2", function(d) { return d.target.y; });
 
-  var link = vis.selectAll("line.link")
-      .data(json.links)
-    .enter().append("svg:line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); })
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+                var node = vis.selectAll("circle.node")
+                    .data(json.nodes)
+                    .enter().append("svg:circle")
+                    .attr("class", "node")
+                    .attr("cx", function(d) { return d.x; })
+                    .attr("cy", function(d) { return d.y; })
+                    .attr("r", 5)
+                    .style("fill", function(d) { return fill(d.group); })
+                    .call(force.drag);
 
-  var node = vis.selectAll("circle.node")
-      .data(json.nodes)
-    .enter().append("svg:circle")
-      .attr("class", "node")
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-      .attr("r", 5)
-      .style("fill", function(d) { return fill(d.group); })
-      .call(force.drag);
+                node.append("svg:title")
+                    .text(function(d) { return d.name; });
 
-  node.append("svg:title")
-      .text(function(d) { return d.name; });
+                force.on("tick", function() {
+                    link.attr("x1", function(d) { return d.source.x; })
+                    .attr("y1", function(d) { return d.source.y; })
+                    .attr("x2", function(d) { return d.target.x; })
+                    .attr("y2", function(d) { return d.target.y; });
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  });
-});
+                node.attr("cx", function(d) { return d.x; })
+                    .attr("cy", function(d) { return d.y; });
+                });
+            });
+        }
+    }
+}(document));

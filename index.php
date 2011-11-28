ef6fd298bc5ac8626e4712bfeaa90f05aa61cfcc
Lukom <lukom_wro@o2.pl>
2011-11-28 20:20:14 +0100

    Fetchowanei do jsona

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 try {
-    $pdo = new PDO('mysql:db=wiki;host=localhost', 'iwi', 'iwi');
-    $stmt = $pdo -> query('SELECT * FROM article');
-    echo '<ul>';
-    foreach($stmt as $row)
-    {
-        echo '<li>'.$row['nazwa'].': '.$row['opis'].'</li>';
+    $pdo = new PDO('mysql:dbname=wiki;host=localhost', 'iwi', 'iwi');
+    $stmt = $pdo->query('SELECT id, name FROM article');
+    $json = array();
+    foreach ($stmt as $row) {
+        $json['nodes'][] = array('id' => $row['id'], 'name' => $row['name']);
     }
-    $stmt -> closeCursor();
-    echo '</ul>';
-
+    $stmt->closeCursor();
+    echo json_encode($json);
 } catch (PDOException $ex) {
     echo 'Connection failed: ' . $e->getMessage();
 }

aff5ea255afb5d11824fd878d57f58ca7f816ff1
Lukom <lukom_wro@o2.pl>
2011-11-28 20:35:32 +0100

    Ajax

--- ajax.php
+++ ajax.php
@@ -1,12 +1,25 @@
 <?php
 try {
     $pdo = new PDO('mysql:dbname=wiki;host=localhost', 'iwi', 'iwi');
-    $stmt = $pdo->query('SELECT id, name FROM article');
+    $articles = $pdo->query('SELECT id, name FROM article LIMIT 50');
+
+    $node_id = array();
     $json = array();
-    foreach ($stmt as $row) {
-        $json['nodes'][] = array('id' => $row['id'], 'name' => $row['name']);
+    foreach ($articles as $article) {
+        $node_id[] = $r
+        $json['nodes'][] = array('id' => $article['id'], 'name' => $article['name']);
     }
-    $stmt->closeCursor();
+    $articles->closeCursor();
+
+    $edges = $pdo->query('
+        SELECT 
+            id_article_from, 
+            id_article_to 
+        FROM article_article 
+        WHERE id_article_from IN ('.join(',', $node_id ).') AND id_article_to IN ('.join(',', $node_id).')');
+    foreach ($edges as $edge) {
+        $json['links'][] = array('source' => $edge['id_article_from'], 'target' => $edge['id_article_to'], 'group' => 1);
+    }
     echo json_encode($json);
 } catch (PDOException $ex) {
     echo 'Connection failed: ' . $e->getMessage();

6689fd6bde276d47914bf05a07305789f335a8f9
Lukom <lukom_wro@o2.pl>
2012-01-07 16:50:28 +0100

    Stop przy alfie

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -135,6 +135,7 @@
                         .attr("y2", function(d) { return d.target.y; });
                     node.attr("cx", function(d) { return d.x; })
                         .attr("cy", function(d) { return d.y; });
+                    force.stop();
                 }
             });
 

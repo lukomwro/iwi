a104cca76e465162a80b612d509de90ed4a41c7a
wojtiku <wojtiku@gmail.com>
2012-01-10 12:03:26 +0100

    pomoc

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -12,8 +12,8 @@
      * Setting up auto complete
      */
     $("#form-search-input").autocomplete(
-        //"http://wikiwizir.yum.pl/ajax/list/", {
-        "/ajax/list/", {
+        "http://wikiwizir.yum.pl/ajax/list/", {
+        //"/ajax/list/", {
             delay:10,
             minChars:2,
             matchSubset:1,
@@ -66,6 +66,23 @@
     }).change();
 
     /**
+     * Help
+     */
+    $('#help-btn').click(function() {
+       $('#help').fadeIn(300, function() {
+           $('#help .popover').fadeIn(500);
+       });
+       return false;
+    });
+
+    $('#help').click(function() {
+        $('#help').fadeOut(500, function() {
+            $('#help .popover').hide();
+        });
+        return false;
+    });
+
+    /**
      * Wiki browser
      */
     $('#wiki-browser').modal({
@@ -95,10 +112,9 @@
             }
             var force, link, node;
             w.location.hash = id;
-            $("#instructions").hide();
             $('#loader').fadeIn(100);
-            //d3.json('json.html?'+id, function(json) {
-            d3.json('/ajax/nodes/'+id, function(json) {
+            d3.json('json.html?'+id, function(json) {
+            //d3.json('/ajax/nodes/'+id, function(json) {
                 vis.selectAll('*').remove();
                 force = d3.layout.force()
                     .charge(GraphSettings.charge)
@@ -200,9 +216,12 @@
     $(d).ready(function(){
         var hash = w.location.hash.substr(1);
         GraphSettings.generateGraph(hash);
-        if (!hash.length) {
-            $("#element-name").text("O programie");
-            $("#instructions").show();
+
+
+        if ($.cookie('first') != '1') {
+            $('#help-btn').click();
+            $.cookie('first', '1', { expires: 60 });
         }
+
     });
 }(window, document));
